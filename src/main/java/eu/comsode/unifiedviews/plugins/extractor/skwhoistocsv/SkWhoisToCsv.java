68eb129bddb3e0790e466e114f42d5b7d4cc9882
Peter Golian <pgo@eea.sk>
2015-05-29 16:24:09 +0200

    Output in RDF format

--- src/main/java/eu/comsode/unifiedviews/plugins/extractor/skwhoistocsv/SkWhoisToCsv.java
+++ src/main/java/eu/comsode/unifiedviews/plugins/extractor/skwhoistocsv/SkWhoisToCsv.java
@@ -3,49 +3,59 @@ package eu.comsode.unifiedviews.plugins.extractor.skwhoistocsv;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.unifiedviews.dataunit.DataUnit;
 import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.files.FilesDataUnit;
-import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
+import eu.unifiedviews.dataunit.rdf.WritableRDFDataUnit;
 import eu.unifiedviews.dpu.DPU;
 import eu.unifiedviews.dpu.DPUException;
-import eu.unifiedviews.helpers.dataunit.copy.CopyHelpers;
 import eu.unifiedviews.helpers.dataunit.files.FilesHelper;
-import eu.unifiedviews.helpers.dataunit.resource.Resource;
-import eu.unifiedviews.helpers.dataunit.resource.ResourceHelpers;
 import eu.unifiedviews.helpers.dpu.config.ConfigHistory;
 import eu.unifiedviews.helpers.dpu.context.ContextUtils;
 import eu.unifiedviews.helpers.dpu.exec.AbstractDpu;
+import eu.unifiedviews.helpers.dpu.rdf.EntityBuilder;
 
 @DPU.AsExtractor
 public class SkWhoisToCsv extends AbstractDpu<SkWhoisToCsvConfig_V1> {
     public static Pattern PATTERN = Pattern.compile("(.*?)   *(.*)");
+
     private static final Logger LOG = LoggerFactory.getLogger(SkWhoisToCsv.class);
+
+    private static final String BASE_URI = "http://localhost/";
+
+    private static final int REPEAT_COUNT = 3;
+
+    private static final int WAIT_IN_MILIS = 1000;
+
+    private Map<String, Integer> keys = new HashMap<String, Integer>();
 
     @DataUnit.AsInput(name = "filesInput")
     public FilesDataUnit filesInput;
 
-    @DataUnit.AsOutput(name = "filesOutput")
-    public WritableFilesDataUnit filesOutput;
+    @DataUnit.AsOutput(name = "rdfOutput")
+    public WritableRDFDataUnit rdfOutput;
 
     public SkWhoisToCsv() {
         super(SkWhoisToCsvVaadinDialog.class, ConfigHistory.noHistory(SkWhoisToCsvConfig_V1.class));
@@ -53,73 +63,115 @@ public class SkWhoisToCsv extends AbstractDpu<SkWhoisToCsvConfig_V1> {
 
     @Override
     protected void innerExecute() throws DPUException {
+        initializeKeysMap();
         try {
             Set<FilesDataUnit.Entry> inputFiles = FilesHelper.getFiles(filesInput);
 
             FilesDataUnit.Entry entry = inputFiles.iterator().next();
             File inputFile = new File(URI.create(entry.getFileURIString()));
-            File outputFile = File.createTempFile("____", FilenameUtils.getExtension(inputFile.getAbsolutePath()), new File(URI.create(filesOutput.getBaseFileURIString())));
             int index = 1;
-            try (BufferedReader inputReader = new BufferedReader(new FileReader(inputFile));
-                    PrintWriter outputWriter = new PrintWriter(new FileWriter(outputFile))) {
+            RepositoryConnection connection = null;
+            try (BufferedReader inputReader = new BufferedReader(new FileReader(inputFile))) {
+                org.openrdf.model.URI graph = rdfOutput.addNewDataGraph("skWhoisRdfData");
+                connection = rdfOutput.getConnection();
+                ValueFactory vf = ValueFactoryImpl.getInstance();
                 String line = null;
                 boolean firstLineInput = true;
-                boolean firstLineOutput = true;
-                List<String> outputHeader = new ArrayList<String>();
                 while ((line = inputReader.readLine()) != null) {
                     if (firstLineInput) {
                         firstLineInput = false;
                         continue;
                     }
-                    List<String> outputLine = new ArrayList<>();
-                    try (Socket whoisClientSocket = new Socket()) {
-                        line = line + "\r\n";
-                        byte[] buffer = line.getBytes("US-ASCII");
-                        whoisClientSocket.connect(new InetSocketAddress("whois.sk-nic.sk", 43));
-                        whoisClientSocket.getOutputStream().write(buffer);
-                        whoisClientSocket.getOutputStream().flush();
-                        List<String> whoisLines = IOUtils.readLines(whoisClientSocket.getInputStream(), "US-ASCII");
-                        int i = 0;
-                        for (String whoisLine : whoisLines) {
-                            if (StringUtils.isBlank(whoisLine) || whoisLine.startsWith("%")) {
-                                continue;
-                            }
-                            Matcher m = PATTERN.matcher(whoisLine);
-                            if (m.matches()) {
-                                if (firstLineOutput) {
-                                    outputHeader.add(m.group(1));
-                                } else {
-                                    if (!outputHeader.get(i).equals(m.group(1))) {
-                                        LOG.error("Unknown column key " + m.group(1) + " with value " + m.group(2) + " found on record of " + line);
-                                    }
+                    String inputId = new String(line);
+                    UUID uuid = UUID.randomUUID();
+                    org.openrdf.model.URI uri = vf.createURI(BASE_URI + uuid.toString());
+                    EntityBuilder eb = new EntityBuilder(uri, vf);
+                    eb.property(RDF.TYPE, vf.createURI(BASE_URI + "Record"));
+                    int failCount = 1;
+                    while (true) {
+                        try (Socket whoisClientSocket = new Socket()) {
+                            line = line + "\r\n";
+                            byte[] buffer = line.getBytes("US-ASCII");
+                            whoisClientSocket.connect(new InetSocketAddress("whois.sk-nic.sk", 43));
+                            whoisClientSocket.getOutputStream().write(buffer);
+                            whoisClientSocket.getOutputStream().flush();
+                            List<String> whoisLines = IOUtils.readLines(whoisClientSocket.getInputStream(), "US-ASCII");
+                            int i = 0;
+                            for (String whoisLine : whoisLines) {
+                                if (StringUtils.isBlank(whoisLine) || whoisLine.startsWith("%")) {
+                                    continue;
                                 }
-                                outputLine.add(m.group(2));
-                            } else {
-                                throw ContextUtils.dpuException(ctx, "FilesFilter.innerExecute.format", whoisLine);
+                                if (StringUtils.contains(whoisLine, "Not found.")) {
+                                    LOG.error("ID " + inputId + " was not found!");
+                                    break;
+                                }
+                                Matcher m = PATTERN.matcher(whoisLine);
+                                if (m.matches()) {
+                                    String key = m.group(1).replaceAll(" ", "-");
+                                    if (!keys.containsKey(key)) {
+                                        LOG.error("Unknown key " + key + " with value " + m.group(2) + " found on record of " + line);
+                                    } else {
+                                        keys.put(key, keys.get(key) + 1);
+                                    }
+                                    eb.property(vf.createURI(BASE_URI + key), m.group(2));
+                                } else {
+                                    throw ContextUtils.dpuException(ctx, "FilesFilter.innerExecute.format", whoisLine);
+                                }
+                                i++;
                             }
-                            i++;
+                            if (whoisLines.size() != 0) {
+                                LOG.info("Reading data for ID " + inputId + ".");
+                                failCount = 1;
+                                break;
+                            } else if (whoisLines.size() == 0 && failCount == REPEAT_COUNT) {
+                                LOG.error("Error reading data for ID " + inputId + "!");
+                                failCount = 1;
+                                break;
+                            } else {
+                                try {
+                                    LOG.warn("Error reading data for ID " + inputId + ". Trying to wait for " + WAIT_IN_MILIS * failCount + " milliseconds.");
+                                    Thread.sleep(WAIT_IN_MILIS * failCount);
+                                    failCount++;
+                                } catch (InterruptedException ex) {
+                                    LOG.error("Sleep for " + WAIT_IN_MILIS * failCount + " milliseconds interrupted.");
+                                }
+                            }
                         }
                     }
-                    if (firstLineOutput) {
-                        outputWriter.println(StringUtils.join(outputHeader, ";"));
-                        firstLineOutput = false;
-                    }
-                    outputWriter.println(StringUtils.join(outputLine, ";"));
+                    connection.add(eb.asStatements(), graph);
                     LOG.info("Done file {}", index);
                     index++;
                 }
             } catch (IOException ex) {
                 ContextUtils.dpuException(ctx, ex, "FilesFilter.innerExecute.exception");
+            } finally {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (RepositoryException ex) {
+                        LOG.warn("Error closing connection.", ex);
+                    }
+                }
             }
-            CopyHelpers.copyMetadata(entry.getSymbolicName(), filesInput, filesOutput);
-            Resource resource = ResourceHelpers.getResource(filesOutput, entry.getSymbolicName());
-            resource.setLast_modified(new Date());
-            resource.setMimetype("text/csv");
-            resource.setSize(outputFile.length());
-            ResourceHelpers.setResource(filesOutput, entry.getSymbolicName(), resource);
-            filesOutput.updateExistingFileURI(entry.getSymbolicName(), outputFile.toURI().toASCIIString());
-        } catch (DataUnitException | IOException ex) {
+            for (Map.Entry<String, Integer> e : keys.entrySet()) {
+                LOG.info("Parameter " + e.getKey() + " was filled " + e.getValue() + " times.");
+            }
+        } catch (DataUnitException | RepositoryException ex) {
             ContextUtils.dpuException(ctx, ex, "FilesFilter.innerExecute.exception");
         }
+    }
+
+    private void initializeKeysMap() {
+        keys.put("Contact-name", 0);
+        keys.put("Organization", 0);
+        keys.put("Legal-form", 0);
+        keys.put("Organization-ID", 0);
+        keys.put("Work-telephone", 0);
+        keys.put("Fax-telephone", 0);
+        keys.put("Work-address", 0);
+        keys.put("Email-address", 0);
+        keys.put("Handle", 0);
+        keys.put("User-delegation", 0);
+        keys.put("Last-record-update", 0);
     }
 }
--- src/test/java/eu/comsode/unifiedviews/plugins/extractor/skwhoistocsv/SkWhoisToCsvTest.java
+++ src/test/java/eu/comsode/unifiedviews/plugins/extractor/skwhoistocsv/SkWhoisToCsvTest.java
@@ -1,22 +1,24 @@
 package eu.comsode.unifiedviews.plugins.extractor.skwhoistocsv;
+
 import java.io.File;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URI;
-import java.util.Set;
+
+import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
 import org.junit.Test;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.Rio;
 
 import cz.cuni.mff.xrg.odcs.dpu.test.TestEnvironment;
-import eu.comsode.unifiedviews.plugins.extractor.skwhoistocsv.SkWhoisToCsv;
-import eu.comsode.unifiedviews.plugins.extractor.skwhoistocsv.SkWhoisToCsvConfig_V1;
-import eu.unifiedviews.dataunit.files.FilesDataUnit;
 import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
-import eu.unifiedviews.helpers.dataunit.files.FilesHelper;
+import eu.unifiedviews.dataunit.rdf.WritableRDFDataUnit;
+import eu.unifiedviews.helpers.dataunit.rdf.RDFHelper;
 import eu.unifiedviews.helpers.dpu.test.config.ConfigurationBuilder;
-
 
 public class SkWhoisToCsvTest {
 
@@ -33,14 +35,17 @@ public class SkWhoisToCsvTest {
         TestEnvironment environment = new TestEnvironment();
 
         // Prepare data unit.
-        WritableFilesDataUnit filesOutput = environment.createFilesOutput("filesOutput");
+        WritableRDFDataUnit rdfOutput = environment.createRdfOutput("rdfOutput", false);
         WritableFilesDataUnit filesInput = environment.createFilesInput("filesInput");
         InputStream inputFileIS = Thread.currentThread().getContextClassLoader()
                 .getResourceAsStream("codes.txt");
         InputStream outputFileIS = Thread.currentThread().getContextClassLoader()
                 .getResourceAsStream("codes_output.txt");
-        String outputFileArray = IOUtils.toString(outputFileIS);
-        
+
+        String s1 = IOUtils.toString(outputFileIS);
+        s1 = s1.substring(s1.indexOf('\n') + 1);
+        s1 = s1.substring(s1.indexOf('\n') + 1);
+
         File tempFile = File.createTempFile("____", "fdsa");
         FileUtils.copyInputStreamToFile(inputFileIS, tempFile);
         try {
@@ -49,14 +54,49 @@ public class SkWhoisToCsvTest {
             // Run.
             environment.run(dpu);
 
-            // Get file iterator.
-            Set<FilesDataUnit.Entry> outputEntries = FilesHelper.getFiles(filesOutput);
+            RepositoryConnection con = rdfOutput.getConnection();
+            StringWriter sw = new StringWriter();
+            con.export(Rio.createWriter(RDFFormat.TURTLE, sw), RDFHelper.getGraphsURIArray(rdfOutput));
+            String s2 = sw.toString();
+            s2 = s2.substring(s2.indexOf('\n') + 1);
+            s2 = s2.substring(s2.indexOf('\n') + 1);
+            Assert.assertEquals(s1.trim(), s2.trim());
+        } finally {
+            // Release resources.
+            environment.release();
+        }
+    }
 
-            // Iterate over files.
-            for (FilesDataUnit.Entry entry : outputEntries) {
-                String  outputContent = FileUtils.readFileToString(new File(new URI(entry.getFileURIString())));
-                Assert.assertEquals(outputFileArray, outputContent);
-            }
+    @Test
+    public void execute2() throws Exception {
+        // Prepare config.
+        SkWhoisToCsvConfig_V1 config = new SkWhoisToCsvConfig_V1();
+
+        SkWhoisToCsv dpu = new SkWhoisToCsv();
+        // Prepare DPU.
+        dpu.configure((new ConfigurationBuilder()).setDpuConfiguration(config).toString());
+
+        // Prepare test environment.
+        TestEnvironment environment = new TestEnvironment();
+
+        // Prepare data unit.
+        WritableRDFDataUnit rdfOutput = environment.createRdfOutput("rdfOutput", false);
+        WritableFilesDataUnit filesInput = environment.createFilesInput("filesInput");
+        InputStream inputFileIS = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("domeny_100.txt");
+        InputStream outputFileIS = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("codes_output.txt");
+
+        File tempFile = File.createTempFile("____", "fdsa");
+        FileUtils.copyInputStreamToFile(inputFileIS, tempFile);
+        try {
+            filesInput.addExistingFile("domeny.txt", URI.create(tempFile.toURI().toASCIIString()).toString());
+
+            // Run.
+            environment.run(dpu);
+
+            RepositoryConnection con = rdfOutput.getConnection();
+            con.export(Rio.createWriter(RDFFormat.TURTLE, System.out), RDFHelper.getGraphsURIArray(rdfOutput));
         } finally {
             // Release resources.
             environment.release();
